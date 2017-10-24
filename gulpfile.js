fa2de599ff011f0180ba3444b3416620cf28d710
Caleb Hardin <>
2017-10-24 13:10:46 -0400

    finalize basic build

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var
     // modules
     gulp = require('gulp'),
     concat = require('gulp-concat'),
-    inject = require('gulp-inject'),
-    merge = require('merge-stream'),
     gfi = require("gulp-file-insert"),
+    path = require('path'),
+    del = require('del'),
+    Q = require('q'),
+    uglify = require('gulp-uglify'),
+    
     // look at merge-stream
 
     // development mode?
@@ -18,70 +21,71 @@ var
     }
 ;
 
-gulp.task('js', function(){
-//    gulp.src(folder.src + '*.js')
-//    .pipe(concat('test.js', {newLine: ','}))
-//    .pipe(gulp.dest(folder.tmp));
-  
-    var x = {
-        "/* insert:snippets */": [folder.src + 'snippets/*.js', 'snippets.js'],
-        "/* insert:files */": [folder.src + 'files/*.js', 'files.js'],
-        "/* insert:reports */": [folder.src + 'reports/*.js', 'reports.js'],
-        "/* insert:callbacks */": [folder.src + 'callbacks/*.js', 'callbacks.js']
-    };
+
+var scriptsPath = 'snippets';
+
+var data = {
+    "/* insert:snippets */": 'snippets',
+    "/* insert:files */": 'files',
+    "/* insert:reports */": 'reports',
+    "/* insert:callbacks */": 'callbacks'
+};
+
+//function getFolders(dir) {
+//    return fs.readdirSync(dir)
+//      .filter(function(file) {
+//        return fs.statSync(path.join(dir, file)).isDirectory();
+//      });
+//}
+
+//(function(i){
+//        result.push(secondFunc(data[i].item)
+//        .then(function(data2) {
+//            data[i].more = data2.item;
+//            return data[i];
+//        }));
+//    })(i);
+
+gulp.task('concat:js', function () {
+    console.log("concat:js runs");
+    var promises = [];
     
+    for (var type in data){
+        var defer = Q.defer();
+        var pipeline = gulp.src(path.join(folder.src, data[type], '/*.js'))
+            .pipe(concat( data[type]+'.js', {newLine: ',\n'} ))
+            .pipe(gulp.dest(folder.tmp));
+        
+        promises.push(defer.promise);
+        
+        (function(pipeline, defer){
+            pipeline.on('end', function () {
+                defer.resolve();
+            });
+        })(pipeline, defer);
+        
+    }
     
-    var files = gulp.src(folder.src + 'snippets/*.js')
-                .pipe(concat('snippets.js', {newLine: ',\n'}))
-                .pipe(gulp.dest(folder.tmp));
+    var all = Q.all(promises);
+    console.log("promises: ", promises);
+    all.then(function(){console.log('success');}, function(){console.error('error');}, function(data){console.log("progress data: ", data);});
+    return all;
+});
+
+gulp.task('inject:js', function(){
+    // point to temp files
+    var replacements = {};
+    for (var type in data){
+        replacements[type] = folder.tmp + data[type] + '.js';
+    }
     
-    console.log("gulp src: ", gulp.src(folder.src + 'snippets/*.js'));
-    
-    gulp.src(folder.src + 'index.html')
-    .pipe(gfi({
-        "/* insert:snippets */": folder.tmp+'snippets.js' //files //folder.src + "1.js"
-    }))
+    return gulp.src(folder.src+"index.html")
+    .pipe(gfi(replacements))
     .pipe(gulp.dest(folder.build));
 });
 
-function concatJSONInsert(data){
-    var x = {};
-    for (var collection in data){
-        gulp.src(data[collection][0])
-            .pipe(concat(data[collection][1], {newLine: ',\n'}))
-            .pipe(gulp.dest(folder.tmp));
-        x[collection] = folder.tmp+data[collection][1];
-    }
-    
-    for (var collection in data){
-        return gulp.src(target)
-        .pipe(gfi({
-            replaceString: tmpName 
-        }))
-        .pipe(gulp.dest(folder.build));
-    }
-    
-    
-}
+gulp.task('remove:tmp', function(){
+    return del([folder.tmp+'*']);
+});
 
-gulp.task('default', ['js']);
-
-//return gulp.src(folder.src + 'index.html')
-//.pipe(inject(
-//    gulp.src('index/base3.html'), {
-//         starttag: '<!-- inject:base3:html -->',
-//         transform: function(filepath, file) {
-//           return file.contents.toString();
-//         }
-//      })
-//)
-
-//gulp.task('test', function() {
-//  var bootstrap = gulp.src('bootstrap/js/*.js')
-//    .pipe(gulp.dest('public/bootstrap'));
-//
-//  var jquery = gulp.src('jquery.cookie/jquery.cookie.js')
-//    .pipe(gulp.dest('public/jquery'));
-//
-//  return merge(bootstrap, jquery);
-//});
+gulp.task('default', gulp.series('concat:js', 'inject:js', 'remove:tmp'));
